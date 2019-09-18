524cc05cf361e7268d18c4dd77ee46acd553aadf
zhaojia <13651270874@126.com>
2019-09-18 14:28:49 +0800

    赵佳发布PHP常用时间\随机数\数组

---
+++ src/util/Arr.php
@@ -0,0 +1,350 @@
+<?php
+
+
+namespace leapad\lib\util;
+
+use ArrayAccess;
+
+class Arr
+{
+
+    /**
+     * Determine whether the given value is array accessible.
+     *
+     * @param  mixed $value
+     * @return bool
+     */
+    public static function accessible($value)
+    {
+        return is_array($value) || $value instanceof ArrayAccess;
+    }
+
+    /**
+     * Determine if the given key exists in the provided array.
+     *
+     * @param  \ArrayAccess|array $array
+     * @param  string|int         $key
+     * @return bool
+     */
+    public static function exists($array, $key)
+    {
+        if ($array instanceof ArrayAccess) {
+            return $array->offsetExists($key);
+        }
+        return array_key_exists($key, $array);
+    }
+
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  \ArrayAccess|array $array
+     * @param  string             $key
+     * @param  mixed              $default
+     * @return mixed
+     */
+    public static function get($array, $key, $default = null)
+    {
+        if (!static::accessible($array)) {
+            return $default;
+        }
+        if (is_null($key)) {
+            return $array;
+        }
+        if (static::exists($array, $key)) {
+            return $array[$key];
+        }
+        foreach (explode('.', $key) as $segment) {
+            if (static::accessible($array) && static::exists($array, $segment)) {
+                $array = $array[$segment];
+            } else {
+                return $default;
+            }
+        }
+        return $array;
+    }
+
+    /**
+     * Get all of the given array except for a specified array of items.
+     *
+     * @param  array        $array
+     * @param  array|string $keys
+     * @return array
+     */
+    public static function except($array, $keys)
+    {
+        static::forget($array, $keys);
+        return $array;
+    }
+
+    /**
+     * Remove one or many array items from a given array using "dot" notation.
+     *
+     * @param  array        $array
+     * @param  array|string $keys
+     * @return void
+     */
+    public static function forget(&$array, $keys)
+    {
+        $original = &$array;
+        $keys = (array)$keys;
+        if (count($keys) === 0) {
+            return;
+        }
+        foreach ($keys as $key) {
+            // if the exact key exists in the top-level, remove it
+            if (static::exists($array, $key)) {
+                unset($array[$key]);
+                continue;
+            }
+            $parts = explode('.', $key);
+            // clean up before each pass
+            $array = &$original;
+            while (count($parts) > 1) {
+                $part = array_shift($parts);
+                if (isset($array[$part]) && is_array($array[$part])) {
+                    $array = &$array[$part];
+                } else {
+                    continue 2;
+                }
+            }
+            unset($array[array_shift($parts)]);
+        }
+    }
+}
+if (!function_exists('array_get')) {
+
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  \ArrayAccess|array $array
+     * @param  string             $key
+     * @param  mixed              $default
+     * @return mixed
+     */
+    function array_get($array, $key, $default = null)
+    {
+        return Arr::get($array, $key, $default);
+    }
+}
+if (!function_exists('e')) {
+
+    /**
+     * Escape HTML special characters in a string.
+     *
+     *
+     * @return string
+     */
+    function e($value)
+    {
+        if (is_array($value)) {
+            $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+        }
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
+    }
+}
+if (!function_exists('array_except')) {
+
+    /**
+     * Get all of the given array except for a specified array of items.
+     *
+     * @param  array        $array
+     * @param  array|string $keys
+     * @return array
+     */
+    function array_except($array, $keys)
+    {
+        return Arr::except($array, $keys);
+    }
+}
+if (!function_exists('hsv2rgb')) {
+    function hsv2rgb($h, $s, $v)
+    {
+        $r = $g = $b = 0;
+
+        $i = floor($h * 6);
+        $f = $h * 6 - $i;
+        $p = $v * (1 - $s);
+        $q = $v * (1 - $f * $s);
+        $t = $v * (1 - (1 - $f) * $s);
+
+        switch ($i % 6) {
+            case 0:
+                $r = $v;
+                $g = $t;
+                $b = $p;
+                break;
+            case 1:
+                $r = $q;
+                $g = $v;
+                $b = $p;
+                break;
+            case 2:
+                $r = $p;
+                $g = $v;
+                $b = $t;
+                break;
+            case 3:
+                $r = $p;
+                $g = $q;
+                $b = $v;
+                break;
+            case 4:
+                $r = $t;
+                $g = $p;
+                $b = $v;
+                break;
+            case 5:
+                $r = $v;
+                $g = $p;
+                $b = $q;
+                break;
+        }
+
+        return [
+            floor($r * 255),
+            floor($g * 255),
+            floor($b * 255)
+        ];
+    }
+}
+if (!function_exists('mb_ucfirst')) {
+    function mb_ucfirst($string)
+    {
+        return mb_strtoupper(mb_substr($string, 0, 1)) . mb_strtolower(mb_substr($string, 1));
+    }
+}
+if (!function_exists('copydirs')) {
+
+    /**
+     * 复制文件夹
+     * @param string $source 源文件夹
+     * @param string $dest   目标文件夹
+     */
+    function copydirs($source, $dest)
+    {
+        if (!is_dir($dest)) {
+            mkdir($dest, 0755, true);
+        }
+        foreach (
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::SELF_FIRST
+            ) as $item
+        ) {
+            if ($item->isDir()) {
+                $sontDir = $dest . DS . $iterator->getSubPathName();
+                if (!is_dir($sontDir)) {
+                    mkdir($sontDir, 0755, true);
+                }
+            } else {
+                copy($item, $dest . DS . $iterator->getSubPathName());
+            }
+        }
+    }
+}
+if (!function_exists('rmdirs')) {
+
+    /**
+     * 删除文件夹
+     * @param string $dirname  目录
+     * @param bool   $withself 是否删除自身
+     * @return boolean
+     */
+    function rmdirs($dirname, $withself = true)
+    {
+        if (!is_dir($dirname)) {
+            return false;
+        }
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($dirname, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($files as $fileinfo) {
+            $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+            $todo($fileinfo->getRealPath());
+        }
+        if ($withself) {
+            @rmdir($dirname);
+        }
+        return true;
+    }
+}
+if (!function_exists('is_really_writable')) {
+
+    /**
+     * 判断文件或文件夹是否可写
+     * @param    string $file 文件或目录
+     * @return    bool
+     */
+    function is_really_writable($file)
+    {
+        if (DIRECTORY_SEPARATOR === '/') {
+            return is_writable($file);
+        }
+        if (is_dir($file)) {
+            $file = rtrim($file, '/') . '/' . md5(mt_rand());
+            if (($fp = @fopen($file, 'ab')) === false) {
+                return false;
+            }
+            fclose($fp);
+            @chmod($file, 0777);
+            @unlink($file);
+            return true;
+        } elseif (!is_file($file) or ($fp = @fopen($file, 'ab')) === false) {
+            return false;
+        }
+        fclose($fp);
+        return true;
+    }
+}
+if (!function_exists('cdnurl')) {
+
+    /**
+     * 获取上传资源的CDN的地址
+     * @param string  $url    资源相对地址
+     * @param boolean $domain 是否显示域名 或者直接传入域名
+     * @return string
+     */
+    function cdnurl($url, $domain = false)
+    {
+        $regex = "/^((?:[a-z]+:)?\/\/|data:image\/)(.*)/i";
+        $url = preg_match($regex, $url) ? $url : \think\Config::get('upload.cdnurl') . $url;
+        if ($domain && !preg_match($regex, $url)) {
+            $domain = is_bool($domain) ? request()->domain() : $domain;
+            $url = $domain . $url;
+        }
+        return $url;
+    }
+}
+if (!function_exists('datetime')) {
+
+    /**
+     * 将时间戳转换为日期时间
+     * @param int    $time   时间戳
+     * @param string $format 日期时间格式
+     * @return string
+     */
+    function datetime($time, $format = 'Y-m-d H:i:s')
+    {
+        $time = is_numeric($time) ? $time : strtotime($time);
+        return date($format, $time);
+    }
+}
+if (!function_exists('format_bytes')) {
+
+    /**
+     * 将字节转换为可读文本
+     * @param int    $size      大小
+     * @param string $delimiter 分隔符
+     * @return string
+     */
+    function format_bytes($size, $delimiter = '')
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        for ($i = 0; $size >= 1024 && $i < 6; $i++) {
+            $size /= 1024;
+        }
+        return round($size, 2) . $delimiter . $units[$i];
+    }
+}
